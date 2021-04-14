3b7b7109dfc6a427bd30d85f814ea838e385d1bf
ThiagoPanini <thipanini94@gmail.com>
2021-04-13 23:33:44 -0300

    Iniciando script de exemplo para classificadores binarios

---
+++ examples/binary_classifier.py
@@ -0,0 +1,16 @@
+"""
+---------------------------------------------------
+------- Exemplos de utilização - mlcomposer -------
+---------------------------------------------------
+Script responsável por consolidar exemplos de
+aplicação relacionadas as funcionalidades presentes
+no módulo ml da biblioteca mlcomposer
+
+Sumário
+-----------------------------------
+
+-----------------------------------
+"""
+
+# Autor: Thiago Panini
+# Data: 13/04/2021
